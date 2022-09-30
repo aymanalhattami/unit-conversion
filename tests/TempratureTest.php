f4b212f567ea272e5bd8a6fdc3adef4ccd388181
developmentayman <114750237+developmentayman@users.noreply.github.com>
2022-09-30 16:45:16 +0300

    Update and rename TempratureTest.php to TemperatureTest.php

--- tests/TempratureTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Aymanalhattami\UnitConversion\Temprature;
-
-it('can convert celicius to fahrenheit', function () {
-    $fahrenheit = Temprature::fromCelicius(100)->toFahrenheit();
-
-    $this->assertEquals(212, $fahrenheit);
-});
