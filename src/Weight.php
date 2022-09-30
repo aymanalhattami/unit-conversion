9f5d60891f834b910df0d47aa83c42ab7d1824ca
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2022-09-30 09:30:01 +0300

    fix: test cs fixe=

--- src/Weight.php
+++ src/Weight.php
@@ -1,23 +1,26 @@
 <?php
 
 namespace Aymanalhattami\UnitConversion;
+use Exception;
 
 class Weight
 {
     private float $kilograms;
 
-    public function __construct(float $kilograms)
-    {
+    public function __construct(float $kilograms){
+
         $this->kilograms = $kilograms;
     }
 
     public static function fromKilograms(float $kilograms): self
     {
         return new static($kilograms);
+
+
     }
 
-    public function toLbs(): float
+    public function toLbs()  : float
     {
-        return round($this->kilograms * 2.204623, 4);
+        return round($this->kilograms    * 2.204623, 4);
     }
 }
